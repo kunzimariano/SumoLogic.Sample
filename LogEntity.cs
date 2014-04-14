8a8403be60af75fa58799357c0e6d01b844b4e08
kunzimariano <kunzi.mariano@gmail.com>
2014-04-14 17:45:01 -0300

    First commit

---
+++ LogEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace ConsoleTest
+{
+    public class LogEntity : TableEntity
+    {
+        public LogEntity()
+        {
+            PartitionKey = "a";
+            RowKey = string.Format("{0:10}_{1}", DateTime.MaxValue.Ticks - DateTime.Now.Ticks, Guid.NewGuid());
+        }
+
+        public string Role { get; set; }
+        public string RoleInstance { get; set; }
+        public int Level { get; set; }
+        public string Message { get; set; }
+        public int Pid { get; set; }
+        public int Tid { get; set; }
+        public int EventId { get; set; }
+        public DateTime EventDateTime
+        {
+            get
+            {
+                return new DateTime(long.Parse(PartitionKey.Substring(1)));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using RestSharp;
+
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        private static DateTimeOffset _lastQuery;
+
+        static void Main(string[] args)
+        {
+            _lastQuery = DateTimeOffset.UtcNow;
+            CloudTable table = GetLogsTable();
+
+            while (true)
+            {
+                string message = GetLogs(table);
+                if (string.IsNullOrWhiteSpace(message))
+                {
+                    Thread.Sleep(TimeSpan.FromMinutes(1));
+                    continue;
+                }
+                StreamToUrl(message);
+            }
+        }
+
+        private static CloudTable GetLogsTable()
+        {
+            string cs = ConfigurationManager.AppSettings["StorageConnectionString"];
+            var storageAccount = CloudStorageAccount.Parse(cs);
+
+            var tableClient = storageAccount.CreateCloudTableClient();
+            //"WADWindowsEventLogsTable"
+            //"WADLogsTable"
+            CloudTable table = tableClient.GetTableReference("WADLogsTable");
+            return table;
+        }
+
+        private static string GetLogs(CloudTable table)
+        {
+            var query = new TableQuery<LogEntity>()
+                .Where(TableQuery.GenerateFilterConditionForDate("Timestamp", QueryComparisons.GreaterThan, _lastQuery));
+
+            var sb = new StringBuilder();
+            foreach (LogEntity entry in table.ExecuteQuery(query))
+            {
+                sb.Append(entry.Message);
+                sb.Append(", ");
+                sb.Append(entry.Timestamp);
+                sb.AppendLine();
+                _lastQuery = entry.Timestamp;
+            }
+
+            return sb.ToString();
+        }
+
+        //TODO: gzip https://service.sumologic.com/ui/help/Default.htm#cshid=4036
+        static void StreamToUrl(string content)
+        {
+            var client = new RestClient();
+            client.BaseUrl = "https://collectors.sumologic.com";
+            var request = new RestRequest();
+            request.Resource = "receiver/v1/http/ZaVnC4dhaV1aogV4rZCv2xBccsc1PBemC6tFssqEyk2bMVkLe1_4We12sWQdqALkwWdt8ncj7NaN1OSILQMhsBPupMs7mAAgIjWDprP4GQjlciCO_17Urw==";
+            request.Method = Method.POST;
+            request.AddFile("someName", Encoding.UTF8.GetBytes(content), "someFileName");
+
+            IRestResponse response = client.Execute(request);
+        }
+    }
+}
